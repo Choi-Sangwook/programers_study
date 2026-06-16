5c6a149ca49a1d345f385e9c1cdd12f86a90e812
Choi_Sangwook <84467227+Choi-Sangwook@users.noreply.github.com>
2026-06-16 14:06:51 +0900

    [level 1] Title: 기사단원의 무기, Time: 623.19 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,16 @@
+def solution(number, limit, power):
+    answer = 0
+
+    for n in range(1, number + 1):
+        count = 0
+
+        for i in range(1, int(n ** 0.5) + 1):
+            if n % i == 0:
+                count += 2
+
+                if i * i == n:
+                    count -= 1
+
+        answer += power if count > limit else count
+
+    return answer
